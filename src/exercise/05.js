06407d33dfe82acc3ae344ad02e6c745ee4ebafb
Carlos Vieira <carlosadrianovieira@gmail.com>
2021-07-21 02:53:17 -0300

    exercise 05 done

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,6 +1,7 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
+import { italic } from 'chalk'
 import * as React from 'react'
 import '../box-styles.css'
 
@@ -12,9 +13,18 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+
+const Box = ({ className = '', style, size, ...props }) => {
+  const boxSize = size ? `box--${size}` : ''
+
+  return (
+    <div className={`box ${boxSize} ${className}`} style={{ fontStyle: italic, ...style }} {...props} />
+  )
+}
+
+const smallBox = <Box size="small" style={{ backgroundColor: 'lightblue' }}>small lightblue box</Box>
+const mediumBox = <Box size="medium" style={{ backgroundColor: 'pink' }}>medium pink box</Box>
+const largeBox = <Box size="large" style={{ backgroundColor: 'orange' }}>large orange box</Box>
 
 function App() {
   return (
